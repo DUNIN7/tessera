31840f80d1ca41bb358aa5f1e5e8588f1e25807c
wedgetail <marvinp@dunin7.com>
2026-02-13 08:59:58 -0500

    Fix jwt.sign: cast entire options object as any

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -100,7 +100,7 @@ router.post('/login', async (req: Request, res: Response) => {
         layers,
       },
       config.jwt.secret,
-      { expiresIn: config.jwt.expiry as any }
+      { expiresIn: config.jwt.expiry } as any
     );
 
     // Audit: login event
